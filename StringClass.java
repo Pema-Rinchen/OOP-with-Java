460f85c79b3ef5b1de6d9c014e4053f8c59a59a0
Pema Rinchen <111570488+Pema-Rinchen@users.noreply.github.com>
2024-03-06 19:30:10 +0530

    Add files via upload

---
+++ StringClass.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class StringClass {
+    public static void main(String[] args) {
+        String str = "radar";
+        System.out.print("Original word: ");
+        System.out.println(str);
+
+        String reversedString = reverseString(str);
+        System.out.println("Reversed word: " + reversedString);
+
+        if (isPalindrome(str)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+
+    public static String reverseString(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
